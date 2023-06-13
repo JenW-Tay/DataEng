ab3abe7c4f74da6c7058b0e8d4e6151d7d8fd92a
Aaron <wen_tay@hotmail.com>
2023-06-13 21:51:38 +0800

    "Update 03 file and run pyspark to read users db"

--- Exercise Files/04_03.py
+++ Exercise Files/04_03.py
@@ -5,32 +5,32 @@ import pyspark
 spark = pyspark.sql.SparkSession \
    .builder \
    .appName("Python Spark SQL basic example") \
-   .config('spark.driver.extraClassPath', "/Users/harshittyagi/Downloads/postgresql-42.2.18.jar") \
+   .config('spark.driver.extraClassPath', "C:/Users/wen_t/Downloads/postgresql-42.6.0.jar") \
    .getOrCreate()
 
 
 ##read table from db using spark jdbc
 movies_df = spark.read \
    .format("jdbc") \
-   .option("url", "jdbc:postgresql://localhost:5432/etl_pipeline") \
+   .option("url", "jdbc:postgresql://localhost:5432/postgres") \
    .option("dbtable", "movies") \
-   .option("user", "<username>") \
-   .option("password", "<password>") \
+   .option("user", "postgres") \
+   .option("password", "1234") \
    .option("driver", "org.postgresql.Driver") \
    .load()
    
 ##add code below
 user_df = spark.read \
    .format("jdbc") \
-   .option("url", "jdbc:postgresql://localhost:5432/etl_pipeline") \
-   .option("dbtable", "_____") \
-   .option("user", "<username>") \
-   .option("password", "<password>") \
-   .option("driver", "______") \
+   .option("url", "jdbc:postgresql://localhost:5432/postgres") \
+   .option("dbtable", "users") \
+   .option("user", "postgres") \
+   .option("password", "1234") \
+   .option("driver", "org.postgresql.Driver") \
    .load()
 
 ##print the users dataframe
-print(_______)
+print(user_df.show())
 
 
 
