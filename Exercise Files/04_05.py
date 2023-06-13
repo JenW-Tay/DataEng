d3e94408d0a8931ab8390769c215262b1941860d
Aaron <wen_tay@hotmail.com>
2023-06-13 22:18:53 +0800

    "Update 05 file to run pyspark transformations"

--- Exercise Files/04_05.py
+++ Exercise Files/04_05.py
@@ -6,39 +6,41 @@ import pyspark.sql
 spark = pyspark.sql.SparkSession \
     .builder \
     .appName("Python Spark SQL basic example") \
-    .config('spark.driver.extraClassPath', "/Users/harshittyagi/Downloads/postgresql-42.2.18.jar") \
+    .config('spark.driver.extraClassPath', "C:/Users/wen_t/Downloads/postgresql-42.6.0.jar") \
     .getOrCreate()
 
 ##read movies table from db using spark
 movies_df = spark.read \
     .format("jdbc") \
-    .option("url", "jdbc:postgresql://localhost:5432/etl_pipeline") \
+    .option("url", "jdbc:postgresql://localhost:5432/postgres") \
     .option("dbtable", "movies") \
-    .option("user", "harshittyagi") \
-   .option("password", "doll") \
+    .option("user", "postgres") \
+    .option("password", "1234") \
     .option("driver", "org.postgresql.Driver") \
     .load()
 
-##read users table from db using spark
+##add code below
 users_df = spark.read \
     .format("jdbc") \
-    .option("url", "jdbc:postgresql://localhost:5432/etl_pipeline") \
+    .option("url", "jdbc:postgresql://localhost:5432/postgres") \
     .option("dbtable", "users") \
-    .option("user", "harshittyagi") \
-   .option("password", "doll") \
+    .option("user", "postgres") \
+    .option("password", "1234") \
     .option("driver", "org.postgresql.Driver") \
     .load()
 
 ## transforming tables
 avg_rating = users_df.groupBy("movie_id").mean("rating")
 
-##join the movies_df and avg_ratings table on id
+print(avg_rating.show())
+
+#join the movies_df and avg_ratings table on id
 df = movies_df.join(avg_rating, movies_df.id == avg_rating.movie_id)
 
 
 ##print all the tables/dataframes
-print(movies_df.show())
-print(users_df.show())
+# print(movies_df.show())
+# print(users_df.show())
 print(df.show())
 
 
